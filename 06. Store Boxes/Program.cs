aa99054db53037c66ebc6a6ee4610c73f49bdf9a
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-10 01:30:38 +0200

    Add files via upload

---
+++ 06. Store Boxes/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _06.Store_Boxes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string info = Console.ReadLine();
+
+            List<Box> boxes = new List<Box>();
+
+            while (info != "end")
+            {
+                string[] infoAsArray = info.Split();
+
+                string serialNumber = infoAsArray[0];
+                string itemName = infoAsArray[1];
+                int itemQuantity = int.Parse(infoAsArray[2]);
+                decimal itemPrice = decimal.Parse(infoAsArray[3]);
+
+                Item item = new Item()
+                {
+                    Name = itemName,
+                    Price = itemPrice,
+                };
+
+                Box box = new Box()
+                {
+                    SerialNumber = serialNumber,
+                    Item = item,
+                    ItemQuantity = itemQuantity,
+                };
+
+                boxes.Add(box);
+
+                info = Console.ReadLine();
+            }
+
+            foreach (var box in boxes.OrderByDescending(x => x.PriceForBox))
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:F2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceForBox:F2}");
+            }
+        }
+    }
+
+    public class Box
+    {
+        public string SerialNumber { get; set; }
+        public Item Item { get; set; }
+        public int ItemQuantity { get; set; }
+        public decimal PriceForBox
+            => ItemQuantity * Item.Price;
+    }
+    public class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
